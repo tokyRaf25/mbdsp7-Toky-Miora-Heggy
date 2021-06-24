7b471b2b6414441bbe0bbaabae9abdccbfa7d0a8
Heggy <HeggyLoyens@gmail.com>
2021-06-24 12:58:15 +0300

    Modification affichage liste

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,29 +1,131 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ParisportService } from './parisport.service';
+import { Parisport , ParisportModele } from './parisport';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategorieService } from './categorie.service';
+import { Categorie } from './categorie.model';
 
-// core components
-import {
-  chartOptions,
-  parseOptions,
-  chartExample1,
-  chartExample2
-} from "../../variables/charts";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
 
-  public datasets: any;
-  public data: any;
-  public salesChart;
-  public clicked: boolean = true;
-  public clicked1: boolean = false;
+  pari_sport:Parisport[];
+  categorie:Categorie[];
+  page: Number=1;
+  limit: Number=10;
+  totalDocs: Number;
+  totalPages: Number;
+  hasPrevPage: boolean;
+  prevPage: Number;
+  hasNextPage: boolean;
+  nextPage: Number;
+  showMsg: boolean = false;
+  constructor(
+	private pariSportService: ParisportService,
+  private categorieService: CategorieService,
+  private route: ActivatedRoute,
+  private router: Router
+  ) {  
+    this.page = this.route.snapshot.params.page;
+  }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(queryParams => {
+      console.log("Dans le subscribe des queryParams")
+      this.page = +queryParams.page || 1;
+      this.limit = +queryParams.limit || 10;
+
+      this.getPariSport();
+      this.getCategorie();
+    });
+    //console.log(this.pari_sport);
+  }
+	
+  getPariSport() {
+    console.log("this.page",this.page);
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>Mandalo ato');
+    this.pariSportService.getPariSportPagine(this.page, this.limit).subscribe(data=>{
+      this.pari_sport = data.docs;
+      this.page = data.page;
+      this.limit = data.limit;
+      this.totalDocs = data.totalDocs;
+      this.totalPages = data.totalPages;
+      this.hasPrevPage = data.hasPrevPage;
+      this.prevPage = data.prevPage;
+      this.hasNextPage = data.hasNextPage;
+      this.nextPage = data.nextPage;
+      console.log(data);
+    });
+  }
+
+  getCategorie(){
+    this.categorieService.getAllCategoriePagine(this.page, this.limit).subscribe(data=>{
+      this.categorie = data.docs;
+      this.page = data.page;
+      this.limit = data.limit;
+      this.totalDocs = data.totalDocs;
+      this.totalPages = data.totalPages;
+      this.hasPrevPage = data.hasPrevPage;
+      this.prevPage = data.prevPage;
+      this.hasNextPage = data.hasNextPage;
+      this.nextPage = data.nextPage;
+      console.log(data);
+    });
+  }
+
+  premierePage() {
+    this.router.navigate(['/pages/dashboard'], {
+      queryParams: {
+        page:1,
+        limit:this.limit,
+      }
+    });
+  }
+
+  pageSuivante() {
+   
+    this.router.navigate(['/pages/dashboard'], {
+      queryParams: {
+        page:this.nextPage,
+        limit:this.limit,
+      }
+    });
   }
 
 
+  pagePrecedente() {
+    this.router.navigate(['/pages/dashboard'], {
+      queryParams: {
+        page:this.prevPage,
+        limit:this.limit,
+      }
+    });
+  }
+
+  dernierePage() {
+    this.router.navigate(['/pages/dashboard'], {
+      queryParams: {
+        page:this.totalPages,
+        limit:this.limit,
+      }
+    });
+  }
+
+  deletePari(parisport:Parisport){
+
+    if(confirm("Etes vous sur de vouloir supprimer ")) {
+      this.pariSportService.deletePariSport(parisport._id).subscribe(data=>{
+        this.getPariSport();
+        this.showMsg= true;
+        this.router.navigate(['/pages/tables/basic-tables'],{replaceUrl:true});
+      });
+    } 
+  }
+
+ 
+  
 }
--- src/app/pages/foot/foot.component.ts
+++ src/app/pages/foot/foot.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ParisportService } from '../dashboard/parisport.service';
+import { Parisport , ParisportModele } from '../dashboard/parisport';
+import { CategorieService } from '../dashboard/categorie.service';
+import { Categorie } from '../dashboard/categorie.model';
 
 @Component({
   selector: 'app-foot',
   templateUrl: './foot.component.html',
-  styleUrls: ['./foot.component.scss']
+  styleUrls: ['./foot.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FootComponent implements OnInit {
 
+  pari_sport:Parisport[];
+  categorie:Categorie[];
+  page: Number=1;
+  limit: Number=10;
+  totalDocs: Number;
+  totalPages: Number;
+  hasPrevPage: boolean;
+  prevPage: Number;
+  hasNextPage: boolean;
+  nextPage: Number;
+  showMsg: boolean = false;
+  constructor(
+	private pariSportService: ParisportService,
+  private categorieService: CategorieService,
+  private route: ActivatedRoute,
+  private router: Router
+  ) {  
+    this.page = this.route.snapshot.params.page;
+  }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(queryParams => {
+      console.log("Dans le subscribe des queryParams")
+      this.page = +queryParams.page || 1;
+      this.limit = +queryParams.limit || 2;
+
+      this.getPariSportByType();
+      this.getCategorie();
+    });
+    //console.log(this.pari_sport);
+  }
+	
+  getPariSportByType() {
+    console.log("this.page",this.page);
+    this.pariSportService.getPariSportByType('60b91a09bb33a3e0bd749715').subscribe(data=>{
+      this.pari_sport = data;
+    });
+  }
+
+  getCategorie(){
+    this.categorieService.getAllCategoriePagine(this.page, this.limit).subscribe(data=>{
+      this.categorie = data.docs;
+      this.page = data.page;
+      this.limit = data.limit;
+      this.totalDocs = data.totalDocs;
+      this.totalPages = data.totalPages;
+      this.hasPrevPage = data.hasPrevPage;
+      this.prevPage = data.prevPage;
+      this.hasNextPage = data.hasNextPage;
+      this.nextPage = data.nextPage;
+      console.log(data);
+    });
+  }
+
+  premierePage() {
+    this.router.navigate(['/pages/tables/basic-tables'], {
+      queryParams: {
+        page:1,
+        limit:this.limit,
+      }
+    });
+  }
+
+  pageSuivante() {
+   
+    this.router.navigate(['/pages/tables/basic-tables'], {
+      queryParams: {
+        page:this.nextPage,
+        limit:this.limit,
+      }
+    });
   }
 
 
+  pagePrecedente() {
+    this.router.navigate(['/pages/tables/basic-tables'], {
+      queryParams: {
+        page:this.prevPage,
+        limit:this.limit,
+      }
+    });
+  }
+
+  dernierePage() {
+    this.router.navigate(['/pages/tables/basic-tables'], {
+      queryParams: {
+        page:this.totalPages,
+        limit:this.limit,
+      }
+    });
+  }
+
+ 
+  
 }
