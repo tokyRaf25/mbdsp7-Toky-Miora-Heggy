34da07e9f2a7a20c5f43a7fbc1e32c92f093c476
Gabrielle <gabrielle.ranomenjanahary@gmail.com>
2021-07-03 20:24:50 +0200

    JWT

--- routes/parie_sports.js
+++ routes/parie_sports.js
@@ -1,6 +1,9 @@
 let PariSport = require('../models/parie_sport');
-
-
+const CategorieService = require ('../service/categorie.service');
+const ChampService = require ('../service/champ_par_categorie_paris.service');
+const CoteService = require ('../service/cote.service');
+var ObjectId = require('mongodb').ObjectId; 
+const jwt = require('jsonwebtoken');
 //Récupérer tous les PariSports (GET), avec paggination
 function getPariSports(req, res){
     var aggregateQuery = PariSport.aggregate(); 
@@ -33,54 +36,112 @@ function getPariSport(req, res) {
     });
 }
 
+function getLastPari(req,res){
+	var aggregateQuery = PariSport.aggregate(); 
+    
+    PariSport.aggregatePaginate(
+    aggregateQuery,
+    {
+	  sort:{_id:-1},
+      limit: 1,
+    },
+    (err, parisport) => {
+      if (err) {
+        res.send(err);
+      }
+      res.send(parisport);
+    }
+  );
+}
+
 // Ajout d'un assignment (POST)
 function postPariSport(req, res) {
-    let pariSport = new PariSport();
-    pariSport.idTypePari = req.body.idTypePari;
-    pariSport.dateDebut = req.body.dateDebut;
-    pariSport.dateFin = req.body.dateFin;
-    pariSport.autres_info = req.body.autres_info;
-    pariSport.status = req.body.status;
-    pariSport.equipes = req.body.equipes;
-    console.log("POST parieSport reçu :");
-    console.log(pariSport);
-  
-    pariSport.save((err) => {
-      if (err) {
-        res.send("cant post pariSport ", err);
-      }
-      res.json({ message: `${pariSport._id} saved!` });
-    });
+   try { 
+	const token = req.body.token; 
+	console.log("token",token);
+	if(!token || typeof token ==='undefined') res.status(403).send("token error");
+   
+	const user =  jwt.verify(token,'supersecret');
+	console.log("user",{user,token});
+		let pariSport = new PariSport();
+		pariSport.idTypePari = req.body.idTypePari;
+		pariSport.dateDuMatch = req.body.dateDuMatch;
+		pariSport.autres_info = req.body.autres_info;
+		pariSport.equipes = req.body.equipes;
+		console.log("POST parieSport reçu :");
+		console.log(pariSport);
+	  
+		pariSport.save((err) => {
+		  if (err) {
+			res.send("cant post pariSport ", err);
+		  }
+		  res.json({ message: `${pariSport._id} saved!` });
+		});
+	}
+	catch(e) { 
+		if(e.name==='TokenExpiredError') { 
+			res.status(403).send({error:"Veuillez se reconnecter , votre session a expiré"});
+		}
+		res.send(e);
+    }
   }
 
 
 // Update d'un PariSport (PUT)
 function updatePariSport(req, res) {
-    console.log("UPDATE recu PariSport : ");
-    console.log(req.body);
-    PariSport.findByIdAndUpdate(
-      req.body._id,
-      req.body,
-      { new: true },
-      (err, pariSport) => {
-        if (err) {
-          console.log(err);
-          res.send(err);
-        } else {
-          res.json({ message: "updated" });
-        }
-      }
-    );
+	try { 
+	    const token = req.body.token; 
+	    console.log("token",token);
+	    if(!token || typeof token ==='undefined') res.status(403).send("token error");
+		const user =  jwt.verify(token,'supersecret');
+	
+		console.log("UPDATE recu PariSport : ");
+		console.log(req.body);
+		PariSport.findByIdAndUpdate(
+		  req.body._id,
+		  req.body,
+		  { new: true },
+		  (err, pariSport) => {
+			if (err) {
+			  console.log(err);
+			  res.send(err);
+			} else {
+			  res.json({ message: "updated" });
+			}
+		  }
+		);
+	}
+	catch(e) { 
+		if(e.name==='TokenExpiredError') { 
+			res.status(403).send({error:"Veuillez se reconnecter , votre session a expiré"});
+		}
+		res.send(e);
+    }
   }
   
   // suppression d'un PariSport (DELETE)
   function deletePariSport(req, res) {
-    PariSport.findByIdAndRemove(req.params.id, (err, pariSport) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json({ message: `${pariSport.autres_info} deleted` });
-    });
+	try { 
+	   const token = req.query.token; 
+	   console.log("token",token);
+	   if(!token || typeof token ==='undefined') res.status(403).send("token error");
+	   
+	   const user =  jwt.verify(token,'supersecret');
+	  
+		  
+		PariSport.findByIdAndRemove(req.params.id, (err, pariSport) => {
+			if (err) {
+			res.send(err);
+		  }
+		  res.json({ message: `${pariSport.autres_info} deleted` });
+		});
+	}
+	catch(e) { 
+		if(e.name==='TokenExpiredError') { 
+			res.status(403).send({error:"Veuillez se reconnecter , votre session a expiré"});
+		}
+		res.send(e);
+    }
   }
 
   function getPariByType(req,res){
@@ -92,6 +153,46 @@ function updatePariSport(req, res) {
       res.json(champ);
     });
   }
+  
+  getDetailPari = async(req,res)=>{
+	try { 
+		 var PariQuery = PariSport.aggregate([
+			{
+				$match : { 
+					_id : new ObjectId(req.params.id)
+				}
+			}
+		 ]);
+		 let resultPari = await PariSport.aggregatePaginate(
+			PariQuery
+		 );
+		 if(resultPari && resultPari.docs && resultPari.docs.length > 0) {
+			for (let i = 0 ; i< resultPari.docs.length ; i++) { 
+					let resultCategorie =  await CategorieService.getCategorie(resultPari.docs[i].idTypePari);
+					resultPari.docs[i].Categorie = resultCategorie;
+					for(let j = 0;j<resultPari.docs[i].Categorie.length;j++){
+						let tempCategorie = {...resultPari.docs[i].Categorie[j]._doc};
+						let resultChamp = await ChampService.getChampByIdCategorie(resultPari.docs[i].Categorie[j]._id);
+						tempCategorie.champ = resultChamp;
+						resultPari.docs[i].Categorie[j]=tempCategorie;
+						for(let count = 0; count<resultPari.docs[i].Categorie[j].champ.length;count++){
+							let tempChamp = {...resultPari.docs[i].Categorie[j].champ[count]._doc};
+							let resultCote = await CoteService.getCoteByIdChamp(resultPari.docs[i].Categorie[j].champ[count]._id,req.params.id);
+							tempChamp.cote = resultCote;
+							resultPari.docs[i].Categorie[j].champ[count] =tempChamp;
+						}
+						
+					}
+			}
+		 }
+		 res.send(resultPari);
+	}
+	catch (e) { 
+		res.send(e);
+		throw e ;
+	}
+ }
+
 
   module.exports = {
     getPariSports,
@@ -100,4 +201,6 @@ function updatePariSport(req, res) {
     updatePariSport,
     deletePariSport,
     getPariByType,
+	getLastPari,
+	getDetailPari
   };
